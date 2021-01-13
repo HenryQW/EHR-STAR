986b67a9dc397c30073f163c35f7585c3dd60c88
Henry <hi@henry.wang>
2021-01-13 03:38:13 +0000

    init: table

---
+++ components/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-navbar toggleable="md" type="dark" variant="primary">
+      <b-navbar-brand href="#"> EHR STAR</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Link 1</b-nav-item>
+          <b-nav-item href="#">Link 2</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container-fluid">
+    <Table />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
