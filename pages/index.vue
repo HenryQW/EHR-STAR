82da0d0bee1707921551421d2de2eeb70b70f4d5
Henry <hi@henry.wang>
2021-05-10 18:43:13 +0100

    feat: update paper title

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,12 @@
   <b-container class="mt-5">
     <b-jumbotron
       header="EHR STAR Browser"
-      lead="EHR STAR: The State-of-the-art in Interactive EHR and PopHR Visualization"
+      lead="EHR STAR: The State-of-the-art in Interactive EHR Visualization"
     >
       <p>
-        This website includes EHR literature and datasets described in in the
+        This website includes EHR literature and datasets described in the
         state-of-the-art (STAR) report, EHR STAR: The State-of-the-art in
-        Interactive EHR and PopHR Visualization.
+        Interactive EHR Visualization.
       </p>
       <b-button variant="primary" to="table">EHR Literature</b-button>
 
